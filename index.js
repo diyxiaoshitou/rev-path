b6f68aaec10302fe9a7d11014c6abf3dfda1106d
diyxiaoshitou <2535477634@qq.com>
2018-10-15 23:35:35 +0800

    Update index.js
    版本位置调整

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ module.exports = (pth, hash) => {
 	if (!(pth && hash)) {
 		throw new Error('`path` and `hash` required');
 	}
-
-	return modifyFilename(pth, (filename, ext) => `${filename}-${hash}${ext}`);
+	return modifyFilename(pth, (filename, ext) => `${filename}-${ext}`);
 };
 
 module.exports.revert = (pth, hash) => {
